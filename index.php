2f6528bef3b18cf6c20f799194de2ffa391007c9
Danish Sehgal <danishsehgal2010@gmail.com>
2019-09-24 22:23:15 +0530

    first push

---
+++ index.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title></title>
+        
+        <!--        Bootstrap-->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        
+        <!--        Font Awesome-->
+        
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+        
+        <!--        OWL Carousel-->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.css">
+        
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.theme.min.css">
+
+        <!--        Stylesheet-->
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+        
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+            <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+      
+    <body> 
+       <nav id="cd-vertical-nav">
+		<ul>
+			<li>
+				<a href="#section1" data-number="1">
+					<span class="cd-dot"></span>
+					<span class="cd-label">Intro</span>
+				</a>
+			</li>
+			<li>
+				<a href="#section2" data-number="2">
+					<span class="cd-dot"></span>
+					<span class="cd-label">About</span>
+				</a>
+			</li>
+			<li>
+				<a href="#section3" data-number="3">
+					<span class="cd-dot"></span>
+					<span class="cd-label">Features</span>
+				</a>
+			</li>
+			<li>
+				<a href="#section4" data-number="4">
+					<span class="cd-dot"></span>
+					<span class="cd-label">Portfolio</span>
+				</a>
+			</li>
+			<li>
+				<a href="#section5" data-number="5">
+					<span class="cd-dot"></span>
+					<span class="cd-label">Pricing</span>
+				</a>
+			</li>
+			<li>
+				<a href="#section6" data-number="6">
+					<span class="cd-dot"></span>
+					<span class="cd-label">Contact</span>
+				</a>
+			</li>
+		</ul>
+	</nav>
+	<a class="cd-nav-trigger cd-img-replace">Open navigation<span></span></a>
+
+	<section id="section1" class="cd-section1">
+		<div>
+            <a class="logo" href="#">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                DS3
+            </a>
+<!--            <img src="images/logo.png">-->
+
+        </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto numquam, totam iusto officia earum perferendis, </p>
+		<a href="#section2" class="cd-scroll-down cd-img-replace">scroll down</a>
+	</section><!-- cd-section -->
+
+	<section id="section2" class="cd-section">
+    
+        <div class="demo">
+            <div class="container"> 
+                <div class="row">
+                    <div id="testimonial-slider" class="owl-carousel">
+                        <div class="testimonial">
+                            <i class="icon fas fa-quote-left"></i>
+                            <p class="description">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto numquam, totam iusto officia earum perferendis, culpa ad atque eveniet praesentium nobis expedita similique beatae tenetur. Distinctio vel tenetur, id cum.
+                            </p>
+                            <div class="testimonial-content">
+                                <div class="pic">
+                                    <img src="images/beats.png" alt="">
+                                </div>
+                                <h3 class="title">Danish Sehgal-</h3>
+                                <span class="post">Web Developer</span>
+                            </div>
+                        </div>
+                        
+                        <div class="testimonial">
+                            <i class="icon fas fa-quote-left"></i>
+                            <p class="description">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto numquam, totam iusto officia earum perferendis, culpa ad atque eveniet praesentium nobis expedita similique beatae tenetur. Distinctio vel tenetur, id cum.
+                            </p>
+                            <div class="testimonial-content">
+                                <div class="pic">
+                                    <img src="images/beats.png" alt="">
+                                </div>
+                                <h3 class="title">Gagan Joshi-</h3>
+                                <span class="post">Web Developer</span>
+                            </div>
+                        </div>
+                        
+                        <div class="testimonial">
+                            <i class="icon fas fa-quote-left"></i>
+                            <p class="description">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto numquam, totam iusto officia earum perferendis, culpa ad atque eveniet praesentium nobis expedita similique beatae tenetur. Distinctio vel tenetur, id cum.
+                            </p>
+                            <div class="testimonial-content">
+                                <div class="pic">
+                                    <img src="images/beats.png" alt="">
+                                </div>
+                                <h3 class="title">Tester-</h3>
+                                <span class="post">Testing</span>
+                            </div>
+                        </div>
+                        
+                        <div class="testimonial">
+                            <i class="icon fas fa-quote-left"></i>
+                            <p class="description">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto numquam, totam iusto officia earum perferendis, culpa ad atque eveniet praesentium nobis expedita similique beatae tenetur. Distinctio vel tenetur, id cum.
+                            </p>
+                            <div class="testimonial-content">
+                                <div class="pic">
+                                    <img src="images/beats.png" alt="">
+                                </div>
+                                <h3 class="title">Tester-</h3>
+                                <span class="post">Testing</span>
+                            </div>
+                        </div>
+                        
+                        <div class="testimonial">
+                            <i class="icon fas fa-quote-left"></i>
+                            <p class="description">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto numquam, totam iusto officia earum perferendis, culpa ad atque eveniet praesentium nobis expedita similique beatae tenetur. Distinctio vel tenetur, id cum.
+                            </p>
+                            <div class="testimonial-content">
+                                <div class="pic">
+                                    <img src="images/beats.png" alt="">
+                                </div>
+                                <h3 class="title">Tester-</h3>
+                                <span class="post">Testing</span>
+                            </div>
+                        </div>
+                        
+                        <div class="testimonial">
+                            <i  class="icon fas fa-quote-left"></i>
+                            <p class="description">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto numquam, totam iusto officia earum perferendis, culpa ad atque eveniet praesentium nobis expedita similique beatae tenetur. Distinctio vel tenetur, id cum.
+                            </p>
+                            <div class="testimonial-content">
+                                <div class="pic">
+                                    <img src="images/beats.png" alt="">
+                                </div>
+                                <h3 class="title">Tester-</h3>
+                                <span class="post">Testing</span>
+                            </div>
+                        </div>icon 
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+	</section><!-- cd-section -->
+
+	<section id="section3" class="cd-section">
+		<p>&ldquo;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto numquam, totam iusto officia earum perferendis, culpa ad atque eveniet praesentium nobis expedita similique beatae tenetur. Distinctio vel tenetur, id cum.&rdquo;</p>
+	</section><!-- cd-section -->
+
+	<section id="section4" class="cd-section2">
+		<div class="wrapper">
+            <div class="card">
+                <h2>1</h2>
+            </div>
+            <div class="card">
+                <h2>2</h2>
+            </div>
+            <div class="card">
+                <h2>3</h2>
+            </div>
+        </div>
+	</section><!-- cd-section -->
+
+	<section id="section5" class="cd-section">
+		<p>&ldquo;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto numquam, totam iusto officia earum perferendis, culpa ad atque eveniet praesentium nobis expedita similique beatae tenetur. Distinctio vel tenetur, id cum.&rdquo;</p>
+	</section><!-- cd-section -->
+
+	<section id="section6" class="cd-section">
+		<p>&ldquo;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto numquam, totam iusto officia earum perferendis, culpa ad atque eveniet praesentium nobis expedita similique beatae tenetur. Distinctio vel tenetur, id cum.&rdquo;</p>
+	</section><!-- cd-section -->
+    </body>
+        <!--        Bootstrap JavaScript-->
+        <script src="https://code.jquery.com/jquery-3.4.0.min.js" integrity="sha256-BJeo0qm959uMBGb65z40ejJYGSgR7REI4+CW1fNKwOg=" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.js"></script>
+        <script type="text/javascript" src="js/index.js"></script>
+</html>
